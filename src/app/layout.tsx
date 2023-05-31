8d73a1ace2cf89d0fba092299358c41568f5c06d
kravening <kajloevesijn@gmail.com>
2023-05-31 22:07:25 +0200

    I don't want to blow out my or other people's eyes so let's atleast enable the dark theme lol.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html data-theme="bumblebee" lang="en">
+    <html data-theme="dark" lang="en">
       <body className={inter.className}>
         <Background />
         {children}
